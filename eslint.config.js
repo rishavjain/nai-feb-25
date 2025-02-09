557a74d8ba5ddccde2cc3a55f504cc10220d5f9e
Rishav Jain <dev.rishavj@gmail.com>
2025-02-09 19:43:20 +0530

    Clean up JSON formatting and add Prettier ignore file
    Consistently formatted JSON files to improve code readability. Added `.prettierignore` to exclude `pnpm-lock.yaml` from formatting. Updated dependencies in `pnpm-lock.yaml` and ensured uniform usage of double quotes.

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,11 @@ import eslintConfigPrettier from 'eslint-config-prettier'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      eslintConfigPrettier,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
